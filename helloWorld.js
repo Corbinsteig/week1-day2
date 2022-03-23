f637cb9cb7669926b3e98fae9bce04bfe1f1eb80
Corbin Steig <corsteig1@gmail.com>
2022-03-23 11:39:59 -0700

    some commit message

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,54 @@
-console.log('hello world')
+//console.log('hello world')
+
+const myName = 'Corbin'
+
+// console.log(myName)
+
+// using let here because age will change
+let age = 23
+
+// console.log(age)
+
+let isCool = true
+
+let nothing = null
+
+let height = undefined
+
+// console.log(height)
+
+height = `5' 6"`
+
+// console.log(height)
+
+let x = 4
+let y = 10
+let z = 7 + 8
+
+let sum = x + y + z
+
+// console.log(sum)
+
+let str1 = "string"
+let str2 = 'string'
+let str3 = `string`
+
+let firstName = 'corbin'
+let lastName = 'steig'
+
+let greeting = "Hello, " + firstName + ' ' + lastName
+
+let greeting2 = `Hello, ${firstName} ${lastName}`
+
+// console.log(greeting2)
+
+const readline = require('readline')
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  reader.question("Howdy, what's your name?", function(answer){
+    console.log("Hi, " + answer)
+    reader.close()  
+  })
